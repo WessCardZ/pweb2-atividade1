bbd28180f3346a5a2092b6658670ebc02b6f5a71
Wesley <wesleyofficial@hotmail.com>
2023-08-09 15:29:39 -0300

    Atividade PROPS

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
-export default function App() {
+// Atividade PROPS
+
+const Campus = ({nome, anoFundacao}) => {
+  return(
+    <Text>O campus {nome} foi fundado em {anoFundacao}</Text>
+  );
+}
+
+const InstitutoFederal = ({sigla, uf}) =>{
+  return(
+    <Text>{sigla} - {uf}</Text>
+  );
+}
+
+function App1() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <InstitutoFederal sigla="IFAL" uf="AL"/>
+      <Campus nome="Maceió" anoFundacao={1909}/>
+      <Campus nome="Rio Largo" anoFundacao={2014}/>
+      <Campus nome="Satuba" anoFundacao={1905}/>
+
+      <InstitutoFederal sigla="IFPE" uf="PE"/>
+      <Campus nome="Palmares" anoFundacao={2014}/>
+      <Campus nome="Garanhuns" anoFundacao={2010}/>
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
+
+// Fim da atividade PROPS
+
+
+
+export default App1;
+
 
 const styles = StyleSheet.create({
   container: {
